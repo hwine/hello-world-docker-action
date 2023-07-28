93e0e2b83fc11e16fbb23d5c3c914fb7f3c79005
Hal Wine <hwine@mozilla.com>
2023-07-28 15:22:58 -0700

    add image debug

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:3.11-slim
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
+# for action debug
+COPY commands.* /
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
-set -x
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> "$GITHUB_OUTPUT"
 echo "python execution"
 bash -c "time python -V"
 
-/bin/pwd
-ls -l
-echo "$0: " "$@"
-
-ls -l /
-
 shopt -s nullglob
-for f in commands.*; do
+# run debug from both image & workspace, if present
+for f in /commands.* commands.*; do
     $f
 done
 
